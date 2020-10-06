7860c23faecd37c18a6331f354e2d5df5f817866
worknet1018 <worknet1018@outlook.com>
2020-10-06 16:37:29 +0800

    chapter2 ----1版本--第一次修改

--- src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -2,6 +2,7 @@ package org.smart4j.chapter2.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.smart4j.chapter2.helper.DatabaseHelper;
 import org.smart4j.chapter2.model.Customer;
 import org.smart4j.chapter2.util.PropsUtil;
 
@@ -17,26 +18,6 @@ import java.util.Properties;
 public class CustomerService {
     private static final Logger LOGGER= LoggerFactory.getLogger(CustomerService.class);
 
-    private static final String DRIVER;
-
-    private static final String URL;
-
-    private static final String USERNAME;
-
-    private static final String PASSWORD;
-
-    static {
-        Properties conf= PropsUtil.loadProps("config.properties");
-        DRIVER= conf.getProperty("jdbc.driver");
-        URL=conf.getProperty("jdbc.url");
-        USERNAME=conf.getProperty("jdbc.username");
-        PASSWORD= conf.getProperty("jdbc.password");
-        try{
-            Class.forName(DRIVER);
-        }catch (ClassNotFoundException e){
-            LOGGER.error("can not load jdbc driver",e);
-        }
-    }
 
     /**
      * 获取客户列表
@@ -49,7 +30,7 @@ public class CustomerService {
             List<Customer> customerList = new ArrayList<Customer>();
         try {
             String sql="SELECT * FROM customer";
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            conn = DatabaseHelper.getConnection();
             PreparedStatement stmt = conn.prepareStatement(sql);
             ResultSet rs=stmt.executeQuery();
             while (rs.next()){
@@ -66,14 +47,7 @@ public class CustomerService {
         } catch (SQLException e) {
             LOGGER.error("execute sql failure",e);
         } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-
-                } catch (SQLException e) {
-                    LOGGER.error("close connection failure", e);
-                }
-            }
+            DatabaseHelper.closeConnection(conn);
         }
         return  customerList;
     }
